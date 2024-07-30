15c0160183a3792e4c106b9a1121aaa702339c6d
mj-una <133172570+mj-una@users.noreply.github.com>
2024-07-29 23:39:23 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,15 @@
-// esto es opcional, pero muuuy recomendable
+//////////////////////////////
+// organizar el codigo js en módulos es 100% opcional (pero muuuy recomendable)
+// para hacerlo así es importante agregarle el atributo type en la etiqueta html:
+// <script type="module" src="./main.js"></script>
+
+// desde el main.js se llama a los primeros fragmentos de códigos (modulos)
+// según el orden que corresponda. y a su vez, desde esos archivos también
+// se llaman a otros módulos a medida que van siendo necesarios
+
+// un tutorial para aprender más sobre ecmascript modules:
+// https://lenguajejs.com/javascript/modulos/que-es-esm/ (son tres paginas,
+// "intro", "exportar", "importar". al inicio y final hay botones pa navegar)
 
 import { sketch0 } from "./sketches/sketch0.js";
 import { sketch1 } from "./sketches/sketch1.js";
@@ -6,20 +17,20 @@ import { sketch1 } from "./sketches/sketch1.js";
 new p5(sketch0);
 new p5(sketch1);
 
-// en vez de crear y ejecutar cada instancia en el mimso archivo
-// es mejor asi, para tener control sobre el orden exacto, q no es
-// solo por toc, sino q tambien afecta el id del canvas q se crea.
-// se llaman "defaultCanvas" + numero de ejecucion. ahora tenemos:
+// es importante el orden exacto en que se crean las instancias (new p5)
+// pq afectará al id del elemtos <canvas> q se crea. ahora tenemos:
 //
 //  <canvas id="defaultCanvas0" class="p5Canvas" etc... ></canvas>
 //  <canvas id="defaultCanvas1" class="p5Canvas" etc... ></canvas>
 //
-// (y esos id son muy importantes pq se usan desde windowResized).
-// 
-// en la consola aparecerá esta advertencia:
+// (estos id son los que se capturan desde windowResized)
+
+
+//////////////////////////////
+// ojo. en la consola aparecerá esta advertencia:
 //
 //  p5.js seems to have been imported multiple times.
 //  Please remove the duplicate import
 //
-// pero es normal, lo dice pq se confunden las instancias.
-// ver los comentarios de sketch0.js
+// es sobre rendimiento. lo dice pq hay mas de una llamada a la librería,
+// cosa que en este caso está bien, pq hay más de una instancia (ver sketch0.js)
