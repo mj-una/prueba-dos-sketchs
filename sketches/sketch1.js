dbade46d79d16a41db5b97415fb2bd5fb31ca259
mj-una <133172570+mj-una@users.noreply.github.com>
2024-07-29 23:42:47 -0300

    Update sketch1.js

--- sketches/sketch1.js
+++ sketches/sketch1.js
@@ -1,43 +1,72 @@
 // este sketch (y cualquier otro mas que se quiera agregar despues)
-// se trabaja como instancia multiple normal, que es tremenda paja.
+// se trabaja como instancia multiple "normal", que es tremenda paja.
 // ejemplo: https://editor.p5js.org/caminofarol/sketches/r609C2cs
-// (cualquier cosa q sea de p5 tiene que estar escrita con "p.")
+// (cualquier palabra q sea de p5 tiene que estar escrita con "p.")
+
+// flag para visibilidad de este dibujo
+import { ocultarSketch1 } from "./sketch0.js";
 
 // todo el bloque se exporta en una funcion flecha
 export const sketch1 = (p) => { // "p" es convencion, se puede ser cualquiera
 
   const id = 1; // identificador sketch
 
+  let f = 50; // frecuencia (pendiente: ajustar para sincronizar con framecount % 255)
+
+  // inicio
   p.setup = () => {
+    
+    // lienzo
     const canvas = p.createCanvas(500, 500);
     canvas.parent("contenedor__sketch--" + id);
     p.windowResized(); // responsive
+
+    // generalidades
     p.stroke(255);
     p.strokeWeight(p.width / 180);
     p.textAlign(CENTER, CENTER);
+    p.textFont("system-ui");
     p.textStyle(BOLD);
   };
 
+  // dibujos
   p.draw = () => {
-    p.clear();
-    p.translate(p.width / 2, p.height / 2);
+
+    // fondo
+    p.clear(); // equivalente a background, pero borra a transparente
+
+    // # ejemplo de comunicación entre distintas instancias
+    // en este caso, usamos una variable que depende del sketch0
+    if (ocultarSketch1) {
+      
+      return; // salida temprana (desercion,,,)
+    }
     
+    // origen
+    p.translate(p.width / 2, p.height / 2);
+    p.rotate(-0.165);    
+
+    // estelas
     p.noFill();
     for (let i = 0; i < 8; i++) {
-      p.circle(0, 0, (sin((p.frameCount - 10 * i) / 50) + 2) * (70 + i * 4));
+      p.circle(0, 0, (sin((p.frameCount - 10 * i) / f) + 2) * (70 + i * 4));
     }
 
+    // circulo color
     p.fill(p.frameCount % 255, 200, 150);
-    p.circle(0, 0, (sin(p.frameCount / 50) + 2) * 70);
+    p.circle(0, 0, (sin(p.frameCount / f) + 2) * 70
+    );
 
+    // texto
     p.push();
     p.fill(255);
     p.noStroke();
-    p.textSize((sin(p.frameCount / 50) + 2) * 25);
-    p.text("uno", 0, 0);
+    p.textSize((sin(p.frameCount / f) + 2) * 25);
+    p.text("UNO", 0, +p.width / 92);
     p.pop();
   };
 
+  // responsive
   p.windowResized = () => {
     const container = document.getElementById("contenedor__sketch--" + id);
     const canvas = document.getElementById("defaultCanvas" + id);
